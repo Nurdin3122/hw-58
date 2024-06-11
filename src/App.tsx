a524d0cdc04435e53f87432d196395c3ff11fcb9
Muhtarov Nurdin <nurdinmuhtarovv@gmail.com>
2024-06-11 15:34:28 +0600

    создал компонент Modal которое при нажатие на кнопку открывается модальное окно

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-
+import Modal from "./components/Modal/Modal";
 import './App.css'
+import {useState} from "react";
 
 const App = () => {
+  const [showModal,setShowModal] = useState(false);
 
   return (
     <>
+        <button className="btn btn-primary w-25" onClick={() => setShowModal(true)}>Click</button>
 
+        <Modal title={'Some kinda modal title'} show={showModal} onClose={() => setShowModal(false)}>
+            This is modal content
+        </Modal>
     </>
   )
 };
---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface Props extends React.PropsWithChildren{
+    show:boolean;
+    title:string;
+    onClose:() => void;
+}
+
+const Modal:React.FC<Props> = ({show,title,children,onClose}) => {
+    return (
+        <>
+            <div className="modal-backdrop show" style={{display: show ? 'block':'none'}}/>
+            <div className="modal show" style={{display: show ? 'block':'none'}} onClick={onClose}>
+                <div className="modal-dialog" onClick={(event) => event.stopPropagation()}>
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5">{title}</h1>
+                            <button className="btn-close" onClick={onClose}></button>
+                        </div>
+                        <div className="modal-body">
+                        {children}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Modal;
